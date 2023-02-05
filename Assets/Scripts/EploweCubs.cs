670b42f2fa35d8e740fff1ee20816124aa09970b
Владимир <51334865+wingofnight@users.noreply.github.com>
2023-02-05 09:03:51 +0300

    Create

---
+++ Assets/Scripts/EploweCubs.cs
@@ -0,0 +1,37 @@
+
+using UnityEngine;
+
+public class EploweCubs : MonoBehaviour
+{
+    public GameObject restartButton, explosion;
+    private bool _collisionSet = false;
+    private void OnCollisionEnter(Collision collision){
+        
+        if(collision.gameObject.tag == "Cube" && !_collisionSet){
+            
+            for(int i = collision.transform.childCount -1; i >= 0; i-- ){
+                Transform child = collision.transform.GetChild(i);
+                child.gameObject.AddComponent<Rigidbody>();
+                child.gameObject.GetComponent<Rigidbody>().AddExplosionForce(70f, Vector3.up, 5f);
+                child.SetParent(null);
+            }
+            restartButton.SetActive(true);
+
+             
+           // Camera.main.transform.position -= new Vector3(0,0,3f);
+            Camera.main.gameObject.AddComponent<CameraShake>();
+
+
+            GameObject newVfx = Instantiate(explosion, new Vector3(collision.contacts[0].point.x, collision.contacts[0].point.y, collision.contacts[0].point.z), Quaternion.identity);
+             Destroy(newVfx, 2.5f);
+             
+            if(PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+            Destroy(collision.gameObject);
+            _collisionSet = true;
+        }
+    }
+
+   
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,219 @@
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class GameController : MonoBehaviour
+{
+   private CubePos nowCube = new CubePos(0,1,0);
+   public float cubeChangePlaceSpeed = 0.5f;
+
+   public Transform cubeToPlace;
+
+   public GameObject cubeToCreate, allCubes, vfx;
+
+   public GameObject[] canvasStartPage;
+
+   private Rigidbody allCubesRB;
+
+    private float camMoveToYPosition, camMoveSpeed = 2f;
+   private bool IsLose, firstCube = false;
+
+    public UnityEngine.Color[]  bgColors;
+    private UnityEngine.Color toCameraColor;
+
+   private List<Vector3> allCubesPosition = new List<Vector3>{
+    new Vector3(0,0,0),
+    new Vector3(1,0,0),
+    new Vector3(-1,0,0),
+    new Vector3(0,1,0),
+    new Vector3(0,0,1),
+    new Vector3(0,0,-1),
+    new Vector3(1,0,1),
+    new Vector3(-1,0,-1),
+    new Vector3(1,0,-1),
+    new Vector3(-1,0,1),
+   };
+
+private int prevCountMaxHorizontal = 0;
+    private Transform mainCam;
+   private Coroutine showCubePlace;
+
+   private void Start()
+   {
+    toCameraColor = Camera.main.backgroundColor;
+      mainCam = Camera.main.transform;
+        camMoveToYPosition = 5.9f + nowCube.y - 1f;
+    allCubesRB = allCubes.GetComponent<Rigidbody>();
+    //StartCoroutine(ShowCubePlace());
+    showCubePlace = StartCoroutine(ShowCubePlace());
+   } 
+
+   private void Update()
+   {
+    if((Input.GetMouseButtonDown(0) || Input.touchCount > 0 || Input.anyKeyDown) &&  cubeToPlace != null && !EventSystem.current.IsPointerOverGameObject()){
+        #if !UNITY_EDITOR
+        if(InversePropertyAttribute.GetTouch(0).phase != TouchPhase.Began)
+        return;
+        #endif 
+
+        if(!firstCube){
+            firstCube = true;
+            foreach(GameObject obj in canvasStartPage){
+               Destroy(obj);     
+            }
+        }
+
+        
+      
+       GameObject newCube = Instantiate(cubeToCreate, cubeToPlace.position, Quaternion.identity) as GameObject;
+
+       newCube.transform.SetParent(allCubes.transform);
+       nowCube.setVector(cubeToPlace.position);
+       allCubesPosition.Add(nowCube.getVector());
+
+       if(PlayerPrefs.GetString("music") != "No")
+       GetComponent<AudioSource>().Play();
+
+       GameObject newVfx = Instantiate(vfx, cubeToPlace.position, Quaternion.identity);
+        Destroy(newVfx, 1.5f);
+
+        allCubesRB.isKinematic = true;
+        allCubesRB.isKinematic = false;
+
+       SpawnPositions();
+       MoveCameraChangeBg();
+      
+    }
+     if( !IsLose && allCubesRB.velocity.magnitude > 0.1f){
+        Destroy(cubeToPlace.gameObject);
+        IsLose = true;
+        StopCoroutine(showCubePlace);
+       }
+
+       mainCam.localPosition =  Vector3.MoveTowards(mainCam.localPosition, new Vector3(mainCam.localPosition.x, camMoveToYPosition, mainCam.localPosition.z), camMoveSpeed * Time.deltaTime);
+       
+
+       if(Camera.main.backgroundColor != toCameraColor)
+       Camera.main.backgroundColor = UnityEngine.Color.Lerp(Camera.main.backgroundColor, toCameraColor, Time.deltaTime / 1.5f);
+   }
+
+   IEnumerator ShowCubePlace(){
+    while(true){
+        SpawnPositions();
+
+        yield return new WaitForSeconds(cubeChangePlaceSpeed);
+    }
+   }
+
+   private void SpawnPositions(){
+    List<Vector3> positions = new List<Vector3>();
+    if(IsPositionEmpty(new Vector3(nowCube.x + 1, nowCube.y, nowCube.z)) && nowCube.x + 1 != cubeToPlace.position.x){
+        positions.Add(new Vector3(nowCube.x + 1, nowCube.y, nowCube.z));
+    }
+    if(IsPositionEmpty(new Vector3(nowCube.x - 1, nowCube.y, nowCube.z))&& nowCube.x - 1 != cubeToPlace.position.x){
+        positions.Add(new Vector3(nowCube.x - 1, nowCube.y, nowCube.z));
+    }
+    if(IsPositionEmpty(new Vector3(nowCube.x, nowCube.y + 1, nowCube.z))&& nowCube.y + 1 != cubeToPlace.position.y){
+        positions.Add(new Vector3(nowCube.x, nowCube.y + 1, nowCube.z));
+    }
+    if(IsPositionEmpty(new Vector3(nowCube.x, nowCube.y - 1, nowCube.z))&& nowCube.y - 1 != cubeToPlace.position.y){
+        positions.Add(new Vector3(nowCube.x, nowCube.y - 1, nowCube.z));
+    }
+    if(IsPositionEmpty(new Vector3(nowCube.x, nowCube.y , nowCube.z + 1))&& nowCube.z + 1 != cubeToPlace.position.z){
+        positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z + 1));
+    }
+    if(IsPositionEmpty(new Vector3(nowCube.x, nowCube.y , nowCube.z - 1))&& nowCube.z - 1 != cubeToPlace.position.z){
+        positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z - 1));
+    }
+
+    if(positions.Count > 1){
+    cubeToPlace.position = positions[UnityEngine.Random.Range(0,positions.Count)];
+    }
+    else if(positions.Count == 0){
+    IsLose = true;
+    }
+    else{
+        cubeToPlace.position = positions[0];
+    }
+
+   }
+
+   private bool IsPositionEmpty(Vector3 targetPos){
+    if(targetPos.y == 0) 
+    {
+        return false;
+    }
+    
+    foreach(Vector3 pos in allCubesPosition){
+        if(pos.x == targetPos.x && pos.y == targetPos.y && pos.z == targetPos.z)
+        { 
+            return false;
+        }
+    }
+
+    return true;
+   }
+
+    private void MoveCameraChangeBg(){
+        int maxX = 0;
+        int maxY = 0;
+        int maxZ = 0;
+        int maxHor;
+        foreach (Vector3 pos in allCubesPosition)
+        {
+            if (Mathf.Abs(Convert.ToInt32(pos.x)) > maxX)
+            {
+                maxX = Convert.ToInt32(pos.x);
+            }
+            if (Convert.ToInt32(pos.y) > maxY)
+            {
+                maxY = Convert.ToInt32(pos.y);
+            }
+            if (Mathf.Abs(Convert.ToInt32(pos.z)) > maxZ)
+            {
+                maxZ = Convert.ToInt32(pos.z);
+            }
+        }
+            camMoveToYPosition = 5.9f + nowCube.y - 1f;
+            maxHor = maxX > maxZ ? maxX : maxZ;
+            if(maxHor % 3 == 0 && prevCountMaxHorizontal!= maxHor){
+                mainCam.localPosition -= new Vector3(0,0,2.5f);
+                prevCountMaxHorizontal = maxHor;
+            }
+
+            if(maxY >= 7){
+                toCameraColor = bgColors[2];
+            }else if(maxY >= 5){
+                toCameraColor = bgColors[1];
+            }
+            else if(maxY >= 2){
+                toCameraColor = bgColors[0];
+            }
+    }
+
+}
+
+
+
+struct CubePos{
+    public int x,y,z;
+
+    public CubePos(int x, int y, int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3 getVector(){
+        return new Vector3(x,y,z);
+    }
+    public void setVector(Vector3 pos){
+        x = Convert.ToInt32(pos.x);
+        y = Convert.ToInt32(pos.y);
+        z = Convert.ToInt32(pos.z);
+    }
+}
